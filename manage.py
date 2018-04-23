bb09c1922dea3e3a738b3154106571ad58d52775
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-04-23 15:31:10 +0300

    modify test user

--- manage.py
+++ manage.py
@@ -5,9 +5,11 @@ from flask_migrate import Migrate, MigrateCommand
 
 #creating app instance
 app = create_app('production')
+app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
